9c6554e13e03b215c445845abbe73787935f9eaa
Duncan Bourquard <bourquardduncan@gmail.com>
2024-03-05 15:30:23 +0100

    Generating number of sessions, but struggles to make right opponents

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ numPlayers = 40
 numTables = int(numPlayers / 4)
 
 players = []
-opponents = []
+playerMatchedWith = []
 tournamentsSession = []
 
 file = open("testTables.txt", "w")
@@ -24,32 +24,42 @@ file = open("testTables.txt", "w")
 def initVars():
     for index in range(1, numPlayers + 1): #Player numbers
         players.append(index)
-        opponents.append([])
+        playerMatchedWith.append([])
     random.shuffle(players)
 
-def printPlayersAndOpponents():
+def printPlayersMatchedWith():
     for index in range(numPlayers): #Player indexs
-        print(str(players[index]) + " : " + str(opponents[index]))
+        print(str(players[index]) + " : " + str(playerMatchedWith[index]))
 
-def setupSession():
+def setupSession(sessionNumber):
     availableplayers = []
     #init
-    file.write("Session 1 : \n")
+    print("Session number : " + str(sessionNumber))
+    file.write("Session " + str(sessionNumber) + ": \n")
     for index in range(numPlayers):
         availableplayers.append(index)
-    file.write(str(availableplayers) + "\n")
-    file.write(str(players) + "\n")
-
+    #file.write(str(availableplayers) + "\n")
+    #file.write(str(players) + "\n")
     #Get tables
     for tableIndex in range(numTables):
         table = selectFourPlayersNeverMatched(availableplayers)
         file.write("\n")
-        addOpponentsToList(table) #Add opponents chosen to the list
+        addOpponentsToList(table) #Add playerMatchedWith chosen to the list
+    file.write("\n\n")
 
 def selectFourPlayersNeverMatched(availableplayers):
     table = []
     for index in range(4):
         selectedPlayerIndex = random.randint(0, len(availableplayers) - 1)
+        #selectedPlayerIndex = len(availableplayers) -1
+        #To add the player, their opponents must not be in the current table.
+        for indexTable in table:
+            for opponentPlayerId in playerMatchedWith[selectedPlayerIndex]:
+                if opponentPlayerId == players[indexTable]:
+                    selectedPlayerIndex -= 1
+        
+
+
         table.append(availableplayers[selectedPlayerIndex])
         file.write(str(players[availableplayers[selectedPlayerIndex]])) # In the final file, we write correct player Number
         if index!=3: file.write("\t")
@@ -63,12 +73,21 @@ def addOpponentsToList(table):
     for playerIndex in table:
         for playerIndex2 in table:
             if(playerIndex != playerIndex2):
-                opponents[playerIndex].append(playerIndex2)
+                playerMatchedWith[playerIndex].append(players[playerIndex2])
 
+def findInArray(array, value):
+    indexValue = -1
+    try:
+        indexValue = array.index(value)
+    except ValueError:
+        indexValue = -1
+    return indexValue
     
 initVars()
-#printPlayersAndOpponents()
-setupSession()
+#printPlayersMatchedWith()
+for sessionNumber in range(numSessions):
+    setupSession(sessionNumber)
+    printPlayersMatchedWith()
 
 
 file.close()
